6ea61dcf29b251de20205c71ac9ceb9a3a13ac52
Paweł J. Wal <pawel.wal@codesthq.com>
2017-03-30 20:32:09 +0200

    MongoDB PoC

--- lib/tyrael/mongodb.rb
+++ lib/tyrael/mongodb.rb
@@ -2,12 +2,34 @@
 
 module Tyrael
   class Mongodb
+    DOC = { name: 'Tyrael', hobbies: %w(hiking tennis fly fishing) }.freeze
+
     def self.call
       new.call
     end
 
     def call
-      'MongoDB'
+      insert
+      collection.drop if collection_count > 10_000
+      collection_count.to_s
+    end
+
+    private
+
+    def mongodb
+      @mongodb ||= Mongo::Client.new('mongodb://127.0.0.1:27017/test')
+    end
+
+    def collection
+      mongodb[:test_collection]
+    end
+
+    def insert
+      collection.insert_one(DOC)
+    end
+
+    def collection_count
+      collection.count
     end
   end
 end
